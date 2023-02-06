92e5973ea8939c31a50443a8d6faf87a3b5b321e
Akanksha-H <aakanksha0499@gmail.com>
2023-02-06 09:53:00 +0530

    Player repeating diceroll untill position is 100

--- src/com/bridgelabz/day4/SankeAndLadder.java
+++ src/com/bridgelabz/day4/SankeAndLadder.java
@@ -7,25 +7,31 @@ public class SankeAndLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_THE_GAME");
         int diceRoll = (int) (Math.round(Math.random() * 10) % 7);
-        int option = (int) (Math.round(Math.random() * 10) % 3);
         int position = 0;
         String playerName = "Mel";
         String playerId = "ID22022";
 
         System.out.println("The point for the player " + playerName + " with ID: " + playerId + " after dice roll is = " + diceRoll);
-        switch (option) {
-            case SNAKE:
-                position -= diceRoll;
-                System.out.println("You have got the snake, position = " + position);
-                break;
+        while (100 > position) {
+            int option = (int) (Math.round(Math.random() * 10) % 3);
+            switch (option) {
+                case SNAKE:
+                    position -= diceRoll;
+                    System.out.println("You have got the snake, position = " + position);
+                    if(position < 0){
+                        position = 0;
+                        System.out.println("You have got the snake before and below 0 so start the game again form position " + position);
+                    }
+                    break;
 
-            case LADDER:
-                position += diceRoll;
-                System.out.println("You have got the ladder, position  = " + position);
-                break;
+                case LADDER:
+                    position += diceRoll;
+                    System.out.println("You have got the ladder, position  = " + position);
+                    break;
 
-            default:
-                System.out.println("You are at the same position = " + position);
+                default:
+                    System.out.println("You are at the same position = " + position);
+            }
         }
     }
 }

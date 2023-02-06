cc1442d778a9b49ee474c2f044900fae1e1553df
Akanksha-H <aakanksha0499@gmail.com>
2023-02-06 09:28:25 +0530

    Diceroll points using random

--- src/com/bridgelabz/day4/SankeAndLadder.java
+++ src/com/bridgelabz/day4/SankeAndLadder.java
@@ -3,10 +3,10 @@ package com.bridgelabz.day4;
 public class SankeAndLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_THE_GAME");
-        int start = 0;
+        int diceRoll = (int) (Math.round(Math.random() * 10) % 7);;
         String playerName = "Mel";
         String playerId = "ID22022";
 
-        System.out.println("Start point for the player " + playerName + " with ID: " + playerId + " is = " + start);
+        System.out.println("The point for the player " + playerName + " with ID: " + playerId + " after dice roll is = " + diceRoll);
     }
 }

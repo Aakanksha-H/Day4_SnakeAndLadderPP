4a683f2eb4eeb2e843726c34b5d073379386df92
Akanksha-H <aakanksha0499@gmail.com>
2023-02-06 09:22:41 +0530

    Introduction of a player with start point

--- src/com/bridgelabz/day4/SankeAndLadder.java
+++ src/com/bridgelabz/day4/SankeAndLadder.java
@@ -3,5 +3,10 @@ package com.bridgelabz.day4;
 public class SankeAndLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_THE_GAME");
+        int start = 0;
+        String playerName = "Mel";
+        String playerId = "ID22022";
+
+        System.out.println("Start point for the player " + playerName + " with ID: " + playerId + " is = " + start);
     }
 }

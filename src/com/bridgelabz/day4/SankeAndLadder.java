4bae35b8269ed009e1abcacb46814bafcc995395
Akanksha-H <aakanksha0499@gmail.com>
2023-02-06 09:34:46 +0530

    Player getting options like snake, ladder or same position

--- src/com/bridgelabz/day4/SankeAndLadder.java
+++ src/com/bridgelabz/day4/SankeAndLadder.java
@@ -1,12 +1,31 @@
 package com.bridgelabz.day4;
 
 public class SankeAndLadder {
+    static final int LADDER = 0;
+    static final int SNAKE = 1;
+
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_THE_GAME");
-        int diceRoll = (int) (Math.round(Math.random() * 10) % 7);;
+        int diceRoll = (int) (Math.round(Math.random() * 10) % 7);
+        int option = (int) (Math.round(Math.random() * 10) % 3);
+        int position = 0;
         String playerName = "Mel";
         String playerId = "ID22022";
 
         System.out.println("The point for the player " + playerName + " with ID: " + playerId + " after dice roll is = " + diceRoll);
+        switch (option) {
+            case SNAKE:
+                position -= diceRoll;
+                System.out.println("You have got the snake, position = " + position);
+                break;
+
+            case LADDER:
+                position += diceRoll;
+                System.out.println("You have got the ladder, position  = " + position);
+                break;
+
+            default:
+                System.out.println("You are at the same position = " + position);
+        }
     }
 }
